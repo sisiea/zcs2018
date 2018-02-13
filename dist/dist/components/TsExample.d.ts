23eb2cc9e20fc6f5977d0111da700a0c0a0e2ab9
sisiea <644022748@qq.com>
2018-02-14 03:34:03 +0800

    update

---
+++ dist/dist/components/TsExample.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import * as React from "react";
+export interface HelloProps {
+    compiler: string;
+}
+export declare class TsExample extends React.Component<HelloProps, {}> {
+    render(): JSX.Element;
+}
---
+++ dist/dist/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+import * as React from "react";
+import "./animate.less";
+import "./indexContent.less";
+export declare class Index extends React.Component<any, any> {
+    render(): JSX.Element;
+}
